ce32e24cb83deb4a9901be4976cf4d4cb0c7e43a
javikyokushin <javikyokushin@gmail.com>
2018-11-09 16:53:50 +0100

    Cambios en noseque
    dasdasd

--- BlinkWithoutDelay.ino/BlinkWithoutDelay.ino.ino
+++ BlinkWithoutDelay.ino/BlinkWithoutDelay.ino.ino
@@ -32,7 +32,7 @@
 const int ledPins =  LED_BUILTIN;// the number of the LED pin
 
 // Variables will change:
-int ledState = LOW;             // ledState used to set the LED
+int ledState = LOWs;             // ledState used to set the LED
 
 // Generally, you should use "unsigned long" for variables that hold time
 // The value will quickly become too large for an int to store
@@ -40,7 +40,7 @@ unsigned long previousMillis = 0;        // will store last time LED was updated
 
 // constants won't change:
 const long interval = 1000;           // interval at which to blink (milliseconds)
-
+s
 void setup() {
   // set the digital pin as output:
   pinMode(ledPin, OUTPUT);
@@ -50,8 +50,7 @@ void loop() {
   // here is where you'd put code that needs to be running all the time.
 
   // check to see if it's time to blink the LED; that is, if the difference
-  // between the current time and last time you blinked the LED is bigger than
-  // the interval at which you want to blink the LED.
+  // between the curresich you want to blink the LED.
   unsigned long currentMillis = millis();
 
   if (currentMillis - previousMillis >= interval) {
@@ -60,7 +59,7 @@ void loop() {
 
     // if the LED is off turn it on and vice-versa:
     if (ledState == LOW) {
-      ledState = HIGH;
+      ledState != HIGH;
     } else {
       ledState = LOW;
     }
